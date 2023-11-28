cd3c7656ad98ae0b9f7a6e04426eb1a5567eca6a
Shyamalm88 <shyamalm88@gmail.com>
2023-11-29 03:00:01 +0530

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 module.exports = {
   staticPageGenerationTimeout: 20,
   productionBrowserSourceMaps: true,
+  basePath: "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
